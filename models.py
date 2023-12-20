9cee9342e3e0f2b304a2d414f0601458cf2385d1
turingelir <fatihdedi18@gmail.com>
2023-12-20 19:13:16 +0300

    UpdateProfileForm (WIH)

--- models.py
+++ models.py
@@ -4,16 +4,36 @@ from wtforms.validators import DataRequired, Length, email
 
 
 class LoginForm(FlaskForm):
-    email = StringField("email", validators=[DataRequired(), validators.Email()] ,render_kw={"class":"email-field"})
-    password = PasswordField("password", validators=[DataRequired(), validators.Length(min=8)], render_kw={"class":"pasword-field"})
+    email = StringField("email", validators=[DataRequired(), validators.Email()], render_kw={"class": "email-field"})
+    password = PasswordField("password", validators=[DataRequired(), validators.Length(min=8)],
+                             render_kw={"class": "pasword-field"})
     submit = SubmitField("submit", render_kw={"class": "submit-field"})
 
 
 class SignUpForm(FlaskForm):
-    name = StringField("name", validators=[DataRequired()] ,render_kw={"class":"input-field"})
-    phone = StringField("phone", validators=[DataRequired(), validators.Length(min=8)] ,render_kw={"class":"input-field"})
-    email = StringField("email", validators=[DataRequired(), validators.Email()] ,render_kw={"class":"input-field"})
-    password = StringField("password", validators=[DataRequired(), validators.Length(min=8)] ,render_kw={"class":"input-field"})
-    address = StringField("address", validators=[DataRequired(), validators.Length(min=8)] ,render_kw={"class":"input-field"})
+    name = StringField("name", validators=[DataRequired()], render_kw={"class": "input-field"})
+    phone = StringField("phone", validators=[DataRequired(), validators.Length(min=8)],
+                        render_kw={"class": "input-field"})
+    email = StringField("email", validators=[DataRequired(), validators.Email()], render_kw={"class": "input-field"})
+    password = StringField("password", validators=[DataRequired(), validators.Length(min=8)],
+                           render_kw={"class": "input-field"})
+    address = StringField("address", validators=[DataRequired(), validators.Length(min=8)],
+                          render_kw={"class": "input-field"})
     submit = SubmitField("submit", render_kw={"class": "submit-field"})
-    
+
+
+# This class is used to update user profile. For empty fields, it will keep the old values.
+class UpdateProfileForm(FlaskForm):
+    name = StringField("name", validators=[DataRequired()], render_kw={"class": "input-field"})
+    phone = StringField("phone", validators=[DataRequired(), validators.Length(min=8)],
+                        render_kw={"class": "input-field"})
+    email = StringField("email", validators=[DataRequired(), validators.Email()], render_kw={"class": "input-field"})
+    password = StringField("password", validators=[DataRequired(), validators.Length(min=8)],
+                           render_kw={"class": "input-field"})
+    address = StringField("address", validators=[DataRequired(), validators.Length(min=8)],
+                          render_kw={"class": "input-field"})
+    submit = SubmitField("submit", render_kw={"class": "submit-field"})
+
+
+
+

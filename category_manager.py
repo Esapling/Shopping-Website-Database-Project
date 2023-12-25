932f5201bffdcc8bb6a2c962a3001f3bcc728d8d
Alperen Demir <109558687+MusaAlperenDemir@users.noreply.github.com>
2023-12-25 08:54:52 +0300

    Update category_manager.py
    Added CRUD operations for Category Class

--- category_manager.py
+++ category_manager.py
@@ -1,6 +1,55 @@
 from database_manager import DatabaseManagement
+import os
+import psycopg
 
 class Category(DatabaseManagement):
     def __init__(self) -> None:
         super().__init__(table_name="category")
 
+    def addCategory(self, category_name, store_id):
+        with psycopg.connect(**self.db_params) as connection:
+            with connection.cursor() as cur:
+                query = f"INSERT INTO {self.table_name} VALUES (%s,%s)"
+                cur.execute(query, (category_name, store_id))
+            connection.commit()
+
+    def deleteCategory(self, category_id):
+        with psycopg.connect(**self.db_params) as connection:
+            with connection.cursor() as cur:
+                query = f"DELETE FROM {self.table_name} WHERE category_id=%s"
+                cur.execute(query, (category_id))
+            connection.commit()
+
+    def updateCategory(self, category_id, category_name,store_id):
+        with psycopg.connect(**self.db_params) as connection:
+            with connection.cursor() as cur:
+                query = f"UPDATE {self.table_name} SET category_name=%s , store_id=%s  WHERE category_id=%s"
+                cur.execute(query, (category_name,store_id,category_id))
+            connection.commit()
+
+    def findCategory(self, category_id):
+        with psycopg.connect(**self.db_params) as connection:
+            with connection.cursor() as cur:
+                query = f"SELECT * FROM {self.table_name} WHERE category_id=%s" 
+                cur.execute(query,(category_id)
+                category = cur.fetchAll()
+                if not category:
+                    print("No category found with given Category Id")
+                    return False
+                else:
+                    print("Category Found")
+                    return category
+
+
+    def findCategoryByStoreIdAndCategoryName(self, category_name, store_id):
+        with psycopg.connect(**self.db_params) as connection:
+            with connection.cursor() as cur:
+                query = f"SELECT * FROM {self.table_name} WHERE category_name=%s AND store_id=%s" 
+                cur.execute(query,(category_name, store_id)
+                category = cur.fetchAll()
+                if not category:
+                    print("No category found with given parameters")
+                    return False
+                else:
+                    print("Category Found")
+                    return category
